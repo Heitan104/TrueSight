427f9b04e5e51fe1b8721c77d95d8b1f5bb25c9c
Heitan Kandasamy <Heitan104@gmail.com>
2025-06-21 23:05:59 -0400

    Manveer's Url code

--- backend/src/utils/url_handler.py
+++ backend/src/utils/url_handler.py
@@ -1,9 +1,16 @@
 import os
+import yt_dlp
 
+def linkDownloader(url, inputPath="spurhacks2025/backend/src/static/uploads"):
 
-def linkDownloader(url, save_path):
-    # TODO: Download video from URL
-
+    path = os.path.abspath(inputPath)
+    os.makedirs(path, exist_ok=True)
     
+    ydl_opts = {
+        'outtmpl': os.path.join(path, '%(title)s.%(ext)s'),
+        'format': 'mp4',
+        'verbose': True
+    }
 
-    pass
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
